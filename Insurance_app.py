e82ceeda6b13555a09495c80ac8f4f465ed34f94
Shekharmeena28 <108546873+Shekharmeena28@users.noreply.github.com>
2023-08-06 14:55:15 +0530

    Update Insurance_app.py

--- Insurance_app.py
+++ Insurance_app.py
@@ -49,7 +49,7 @@ try:
         st.markdown("<h2 style='text-align: center;'>Health Insurance Cost Prediction Using ML</h2>", unsafe_allow_html=True)
 
         # Load the ML model
-        model = joblib.load("Eda and Model building/GB_MODEL.joblib")
+        model = joblib.load("/absolute/path/to/GB_MODEL.joblib")
 
         # Input fields for user input
         p1 = st.number_input("Enter Your Age", min_value=18, max_value=100, value=25)
